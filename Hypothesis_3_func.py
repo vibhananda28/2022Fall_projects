7aea09030568be07876f0c20a5bc2fe0c9ea91d3
Deepanshu Malhotra <deepanshumalhotra@wirelessprv-10-195-213-136.near.illinois.edu>
2022-12-03 16:16:44 -0600

    Added hypothesis 3 and modified hypothesis 2

--- Hypothesis_3_func.py
+++ Hypothesis_3_func.py
@@ -9,13 +9,15 @@ from scipy.stats import pearsonr
 import datetime
 from IPython.display import display, HTML
 
-def clean_data1(df1):
+def clean_data_hyp3(df1):
     alist = df1["Country"].unique()
     for i1 in alist:
         df2 = df1.loc[df1["Country"]==i1]
-        len_recovery = len(df2["Recovery%"].unique())
+        len_population = len(df2["Population_total"].unique())
         len_municipal = len(df2["Municipal_waste_generated_percapita"].unique())
-        if(len_recovery<3 or len_municipal<3):
+        len_forest = len(df2["Forest_area"].unique())
+        
+        if(len_population<3 or len_municipal<3 or len_forest<3):
             del_row = df1.loc[df1["Country"]==i1].index
             df1 = df1.drop(del_row)
     
@@ -76,12 +78,12 @@ def time_series_subplots(ts1, variable):
     
     return fig, axs
 
-def calculate_correlation(df1):
+def calculate_correlation_hyp3(df1):
     alist = []
     for i1 in df1['Country'].unique():
-        df2 = df1.loc[df1['Country']==i1][['Country', "Recovery%", "Municipal_waste_generated_percapita"]]
+        df2 = df1.loc[df1['Country']==i1][['Country', "Forest_area", "Municipal_waste_generated_percapita"]]
         df2 = df2.dropna()
-        corr, _ = pearsonr(df2["Recovery%"], df2["Municipal_waste_generated_percapita"])
+        corr, _ = pearsonr(df2["Forest_area"], df2["Municipal_waste_generated_percapita"])
         alist.append([i1, corr])
     
     return alist
@@ -101,7 +103,7 @@ def country_groups(alist):
 
     return alist1, alist2, alist3
 
-def group_time_series(ts1, corr_list, figure_size):
+def group_time_series_hyp3(ts1, corr_list, figure_size):
     tot_num = len(corr_list)*2
     num_col = 4 # number of figures to display in one row
     #Calculating number of rows
@@ -118,14 +120,14 @@ def group_time_series(ts1, corr_list, figure_size):
     for key1 in corr_list:
         ts2 = ts1[ts1["Country"]==key1[0]]
         x1 = ts2["Year"]
-        y1 = ts2["Recovery%"]
+        y1 = ts2["Forest_area"]
         y2 = ts2["Municipal_waste_generated_percapita"]
 
         i1 = cnt//num_col
         j1 = cnt%num_col
 
         axs[i1,j1].set_title(key1[0])
-        axs[i1,j1].set_ylabel("Recovery%")
+        axs[i1,j1].set_ylabel("Forest_area")
         axs[i1,j1].set_xlabel("Time")
         axs[i1,j1].scatter(x1, y1)
         
